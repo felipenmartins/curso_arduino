ab140bfa4aa1efe9ca14de44fd670f45019c51b1
Felipe Nascimento Martins <felipe.n.martins@gmail.com>
2014-05-02 17:08:09 -0300

    Create Pratica_07_Medindo_Temperatura.ino
    // Curso de Introdução a Sistemas Embarcados com Arduino
    // ISA-ES - MAR-ABR/2014
    // Prof. Felipe N. Martins
    // www.nossosrobos.blogspot.com
    //
    // Prática 7 - Medindo Temperatura
    // Objetivo: fazer a leitura do valor obtido por um sensor de
    // temperatura e enviá-lo ao computador via porta USB
    //
    // Hardware adicionado:
    // Sensor de temperatura TMP36 - pino A0

---
+++ Pratica_07_Medindo_Temperatura.ino
@@ -0,0 +1,25 @@
+// Curso de Introdução a Sistemas Embarcados com Arduino
+// ISA-ES - MAR-ABR/2014
+// Prof. Felipe N. Martins
+// www.nossosrobos.blogspot.com
+//
+// Prática 7 - Medindo Temperatura
+// Objetivo: fazer a leitura do valor obtido por um sensor de
+// temperatura e enviá-lo ao computador via porta USB
+//
+// Hardware adicionado:
+// Sensor de temperatura TMP36 - pino A0
+
+float temperatura;
+
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  int sensor = analogRead(A0);
+  temperatura = (float)sensor * 0.45;
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.println("C.");  
+  delay(500);
+}
