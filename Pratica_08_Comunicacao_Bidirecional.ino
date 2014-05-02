2de5a89d143ccd9c66712a009bfbbb9cb24e05c6
Felipe Nascimento Martins <felipe.n.martins@gmail.com>
2014-05-02 17:10:01 -0300

    Create Pratica_08_Comunicacao_Bidirecional.ino
    // Prática 8 - Comunicação Bidirecional
    // Objetivo: fazer comunicação bidirecional entre o Arduino e o 
    // computador. O Arduino solicita ao usuário que entre com um
    // número entre 0 e 9 e apresenta a tabuada desse número.
    //

---
+++ Pratica_08_Comunicacao_Bidirecional.ino
@@ -0,0 +1,40 @@
+// Curso de Introdução a Sistemas Embarcados com Arduino
+// ISA-ES - MAR-ABR/2014
+// Prof. Felipe N. Martins
+// www.nossosrobos.blogspot.com
+//
+// Prática 8 - Comunicação Bidirecional
+// Objetivo: fazer comunicação bidirecional entre o Arduino e o 
+// computador. O Arduino solicita ao usuário que entre com um
+// número entre 0 e 9 e apresenta a tabuada desse número.
+//
+// Hardware adicionado:
+// 
+
+int i, numero;
+void setup() {
+  Serial.begin(9600); 
+}
+void loop() {
+  while (true) {
+    Serial.print("Entre com um numero: "); 
+    while (Serial.available()==0);    
+    numero = Serial.read();     
+      if (numero>='0' && numero<='9')
+      Serial.println(numero-'0');        
+    else {
+      Serial.println("O valor deve ser numerico!");   
+      continue;                                   
+    }
+    numero-='0'; 
+    for (i = 0; i <= 10; i++) {                                        
+      Serial.print(numero);
+      Serial.print(" x ");
+      Serial.print(i);
+      Serial.print(" = ");
+      Serial.println(numero*i);
+    }
+    Serial.println("");    
+  }
+}
+
