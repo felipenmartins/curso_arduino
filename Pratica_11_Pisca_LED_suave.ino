92fc725645087b99d0d73a54d0a1bea64fbc657c
Felipe Nascimento Martins <felipe.n.martins@gmail.com>
2014-05-02 17:11:49 -0300

    Create Pratica_11_Pisca_LED_suave.ino
    // Prática 11 - Controle de Brilho com PWM
    // Objetivo: Piscar o LED do pino 13 suavemente usando PWM.

---
+++ Pratica_11_Pisca_LED_suave.ino
@@ -0,0 +1,26 @@
+// Curso de Introdução a Sistemas Embarcados com Arduino
+// ISA-ES - MAR-ABR/2014
+// Prof. Felipe N. Martins
+// www.nossosrobos.blogspot.com
+//
+// Prática 11 - Controle de Brilho com PWM
+// Objetivo: Piscar o LED do pino 13 suavemente usando PWM.
+//
+// Hardware adicionado:
+
+
+int brilho = 0; // brilho do LED
+int variacao = 5; // quanto varia o brilho
+
+void setup() { 
+  pinMode(13, OUTPUT);
+} 
+
+void loop() { 
+  analogWrite(13, brilho); 
+  brilho = brilho + variacao;
+  if (brilho == 0 || brilho == 255) {
+    variacao = -variacao; 
+  } 
+  delay(30); 
+}
